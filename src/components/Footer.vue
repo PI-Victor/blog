0212e56094ef7f8a281e4484f8ff81b022c26404
Victor Palade <victor@cloudflavor.io>
2020-01-11 15:05:23 +0100

    initial commit
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer dark padless>
+    <v-card-title class="teal">
+      <span>Victor Palade</span>
+    </v-card-title>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <Preview />
+    <Footer />
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Footer from "@/components/Footer";
+import Preview from "@/components/Preview";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Preview,
+    Footer
   }
 };
 </script>
